55a6540644a7758ede27ea99073d4c7d8429685e
palatin <Igor_shamin@mail.ru>
2017-05-04 21:06:29 +0300

    Revert "Edit question logic changes"

--- app/src/main/java/example/com/powerinterview/core/PowerInterviewApp.java
+++ app/src/main/java/example/com/powerinterview/core/PowerInterviewApp.java
@@ -1,30 +1,14 @@
 package example.com.powerinterview.core;
 
-
 import android.app.Application;
 
-
-import com.orm.SugarContext;
 import com.squareup.leakcanary.LeakCanary;
-
-import example.com.powerinterview.components.DaggerInterviewComponent;
-import example.com.powerinterview.components.InterviewComponent;
-import example.com.powerinterview.factories.InterviewModule;
-
-import example.com.powerinterview.components.AuthComponent;
-import example.com.powerinterview.components.DaggerAuthComponent;
-import example.com.powerinterview.factories.AuthModule;
 
 /**
  * Created by Игорь on 30.03.2017.
  */
 
 public class PowerInterviewApp extends Application {
-
-    private InterviewComponent interviewComponent;
-    private int permission = 1000;
-
-    private AuthComponent authComponent;
 
     @Override public void onCreate() {
         super.onCreate();
@@ -34,21 +18,6 @@ public class PowerInterviewApp extends Application {
         }
         LeakCanary.install(this);
 
-        authComponent = DaggerAuthComponent.builder().authModule(new AuthModule()).build();
-
-        interviewComponent = DaggerInterviewComponent.builder().interviewModule(new InterviewModule()).build();
-
-        SugarContext.init(getApplicationContext());
-
     }
-
-
-    public InterviewComponent getInterviewComponent() {
-        return interviewComponent;
-    }
-    public AuthComponent getAuthComponent() {
-        return authComponent;
-    }
-
 
 }
